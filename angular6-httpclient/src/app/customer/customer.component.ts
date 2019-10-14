d8f8cc011558e1fda331012982db62f1908f6562
usama-jamil <usamajamil006@gmail.com>
2019-10-14 17:51:44 +0500

    Customer component

--- angular6-httpclient/src/app/customer/customer.component.ts
+++ angular6-httpclient/src/app/customer/customer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Customer } from "../customer";
+import { CustomerService } from "../customer.service";
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"]
 })
 export class CustomerComponent implements OnInit {
+  customers: Customer[];
+  constructor(private customerService: CustomerService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getCustomers();
   }
 
+  getCustomers() {
+    return this.customerService.getCustomers().subscribe(customers => {
+      console.log(customers);
+      this.customers = customers;
+    });
+  }
 }
