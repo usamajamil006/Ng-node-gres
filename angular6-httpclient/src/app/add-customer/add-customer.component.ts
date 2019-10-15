deb58274308cb49f09669fadc2fe4b969cafda95
usama-jamil <usamajamil006@gmail.com>
2019-10-15 12:39:07 +0500

    Adding New Customer

--- angular6-httpclient/src/app/add-customer/add-customer.component.ts
+++ angular6-httpclient/src/app/add-customer/add-customer.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Customer } from "../customer";
+import { CustomerService } from "../customer.service";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-add-customer',
-  templateUrl: './add-customer.component.html',
-  styleUrls: ['./add-customer.component.css']
+  selector: "app-add-customer",
+  templateUrl: "./add-customer.component.html",
+  styleUrls: ["./add-customer.component.css"]
 })
 export class AddCustomerComponent implements OnInit {
+  customer = new Customer();
+  submitted = false;
+  constructor(
+    private customerService: CustomerService,
+    private location: Location
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  newCustomer(): void {
+    this.submitted = false;
+    this.customer = new Customer();
   }
 
+  addCustomer(): void {
+    this.submitted = true;
+    this.save();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private save(): void {
+    this.customerService.addCustomer(this.customer).subscribe();
+  }
 }
