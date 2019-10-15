6b9f05717373da3fb4111ad47bf429d0800659bb
usama-jamil <usamajamil006@gmail.com>
2019-10-15 13:40:51 +0500

    Creating route for Customer

---
+++ node-gres-api/route/customer.route.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+  const customer = require("../controller/customer.controller");
+
+  //Create A new Customer
+  app.post("/api/customers", customer.create);
+  // Retrieve all Customer
+  app.get("/api/customers", customer.findAll);
+  // Retrieve a single Customer by Id
+  app.get("/api/cusotmers/:id", customer.findById);
+  // Update a Customer with Id
+  app.put("/api/customers", customer.update);
+  // Delete a Customer with Id
+  app.delete("/api/customer/:id", customer.delete);
+};
